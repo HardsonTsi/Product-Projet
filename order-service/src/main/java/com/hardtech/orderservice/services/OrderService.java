ade3b3f6c8a310c1b9001f953884a884db03db8f
TESSI Hardson <hardsontessi2@gmail.com>
2023-01-18 16:26:39 +0100

    api gateway

--- order-service/src/main/java/com/hardtech/orderservice/services/OrderService.java
+++ order-service/src/main/java/com/hardtech/orderservice/services/OrderService.java
@@ -44,7 +44,6 @@ public class OrderService {
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
-
         return "Order placed Successfuly";
     }
 
